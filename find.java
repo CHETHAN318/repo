f766b470f8b04f329cc95a7804c974b7821b0462
xyz <xyz@example.com>
2023-01-19 16:15:08 +0530

    Added two files :

---
+++ find.java
@@ -0,0 +1,7 @@
+class Sum {
+public static void main(String[] args) {
+    int num1 = 500, num2 = 80000;
+    int sum = num1 + num2;
+    System.out.println("The sum  of two numbers is : "+sum);
+}
+}
