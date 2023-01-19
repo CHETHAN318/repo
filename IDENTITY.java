f0b12e5a27a0c987064eed21582f94d4af22f199
xyz <xyz@example.com>
2023-01-19 16:04:23 +0530

    renamed the information in the file Identity.java

--- IDENTITY.java
+++ IDENTITY.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Identity {
+public class IDENTITY {
 	public static void main(String[] args) {
 		Scanner s=new Scanner(System.in);
 		System.out.println("Enter the n value");
